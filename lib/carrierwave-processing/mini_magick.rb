bc69bb9996afe7713e13f265e2d97d5cac16e359
Petrik de Heus <petrik@deheus.net>
2019-11-13 21:07:36 +0100

    Update for ImageProcessing gem
    See: https://github.com/carrierwaveuploader/carrierwave/pull/2298/files

--- lib/carrierwave-processing/mini_magick.rb
+++ lib/carrierwave-processing/mini_magick.rb
@@ -3,7 +3,7 @@ module CarrierWave
     module MiniMagick
       # Strips out all embedded information from the image
       def strip
-        manipulate! do |img|
+        minimagick! do |img|
           img.strip
           img = yield(img) if block_given?
           img
@@ -12,7 +12,7 @@ module CarrierWave
 
       # Reduces the quality of the image to the percentage given
       def quality(percentage)
-        manipulate! do |img|
+        minimagick! do |img|
           img.quality(percentage.to_s)
           img = yield(img) if block_given?
           img
@@ -22,7 +22,7 @@ module CarrierWave
       # Auto Orients images
       # may not work if using stip, since orientation is in the EXIF data
       def rotate
-        manipulate! do |img|
+        minimagick! do |img|
           img.auto_orient
           img = yield(img) if block_given?
           img
@@ -31,8 +31,8 @@ module CarrierWave
 
       # Adds a watermark at the center
       def watermark(source_watermark)
-        manipulate! do |img|
-          watermark = ::MiniMagick::Image.open(source_watermark)
+        minimagick! do |img|
+          watermark = ::MiniMagick::Image.new(source_watermark)
           watermark.resize img[:dimensions].join('x')
           img = img.composite(watermark, 'png') do |c|
             c.gravity "center"
